d4b72e98c0875edc7786f8df15f34bee47289195
imranovvv <imranhafizuddin@gmail.com>
2023-10-30 18:58:54 +0800

    Updated New Group page

--- lib/screens/joingroup.dart
+++ lib/screens/joingroup.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:kongsi/components/appbar.dart';
 
 class JoinGroup extends StatefulWidget {
@@ -11,47 +9,79 @@ class JoinGroup extends StatefulWidget {
 }
 
 class _JoinGroupState extends State<JoinGroup> {
-  TextEditingController nameController = TextEditingController();
-  TextEditingController emailController = TextEditingController();
+  List<String> items = [];
+  TextEditingController textController = TextEditingController();
+
+  void addMember(String name) {
+    setState(() {
+      items.add(name);
+      textController.clear();
+    });
+  }
+
+  void removeMember(int index) {
+    setState(() {
+      items.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const CustomAppBar(showLogoutButton: false),
-      body: Column(
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              AppBar(
-                title: const Text(
-                  'Join Group',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: items.length + 1,
+              itemBuilder: (context, index) {
+                if (index < items.length) {
+                  return ListTile(
+                    contentPadding: const EdgeInsets.only(left: 20.0),
+                    title: Text(items[index]),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.clear),
+                      onPressed: () {
+                        removeMember(index);
+                      },
+                    ),
+                  );
+                } else {
+                  return ListTile(
+                    contentPadding: const EdgeInsets.only(left: 20.0),
+                    title: TextField(
+                      controller: textController,
+                      decoration: const InputDecoration(
+                        hintText: 'Enter name', // Add hint text here
+                      ),
+                    ),
+                    trailing: Padding(
+                      padding: const EdgeInsets.only(
+                          right: 0.0), // Adjust the padding value here
+                      child: IconButton(
+                        icon: const Icon(Icons.add),
+                        onPressed: () {
+                          addMember(textController.text);
+                        },
+                      ),
+                    ),
+                  );
+                }
+              },
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.all(16.0), // Adjust the padding as needed
+              child: ElevatedButton(
+                onPressed: () {
+                  // Add your save action here
+                },
+                child: Text('Save'),
               ),
-              CupertinoTextField(
-                placeholder: 'Name',
-                controller: nameController,
-                keyboardType: TextInputType.text,
-                padding: const EdgeInsets.all(10.0),
-                clearButtonMode: OverlayVisibilityMode.editing,
-                style: GoogleFonts.poppins(),
-              ),
-              const SizedBox(height: 16.0),
-              CupertinoTextField(
-                placeholder: 'Email',
-                controller: emailController,
-                keyboardType: TextInputType.emailAddress,
-                padding: const EdgeInsets.all(10.0),
-                clearButtonMode: OverlayVisibilityMode.editing,
-                style: GoogleFonts.poppins(),
-              ),
-              const SizedBox(height: 16.0),
-            ],
-          ),
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/screens/newgroup.dart
+++ lib/screens/newgroup.dart
@@ -14,110 +14,205 @@ class NewGroup extends StatefulWidget {
 class _NewGroupState extends State<NewGroup> {
   TextEditingController nameController = TextEditingController();
   TextEditingController emailController = TextEditingController();
+  final List<String> items = [];
+  final TextEditingController textController = TextEditingController();
+
+  void addMember(String name) {
+    setState(() {
+      items.add(name);
+      textController.clear();
+    });
+  }
+
+  void removeMember(int index) {
+    setState(() {
+      items.removeAt(index);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const CustomAppBar(showLogoutButton: false),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 1,
+              height: MediaQuery.of(context).size.width * 0.1,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xff10416d),
+                  elevation: 0,
+                ),
+                onPressed: () {},
+                child: const Text(
+                  "Save",
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
       body: Column(
         children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              AppBar(
-                centerTitle: true,
-                title: const Text(
-                  'New Group',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
+          AppBar(
+            centerTitle: true,
+            title: const Text(
+              'New Group',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 30, right: 30, top: 20),
+              child: Column(
+                children: [
+                  CupertinoTextField(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5.0),
+                      color: Colors.white,
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.grey, // You can set the shadow color
+                          offset:
+                              Offset(0, 1), // Specify the offset of the shadow
+                          blurRadius: 4, // Specify the blur radius
+                        ),
+                      ],
+                    ),
+                    placeholder: 'Title',
+                    controller: nameController,
+                    keyboardType: TextInputType.text,
+                    style: GoogleFonts.poppins(),
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 16, vertical: 6),
                   ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 30, right: 30, top: 20),
-                child: Column(
-                  children: [
-                    CupertinoTextField(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(30.0),
-                        color: Colors.white,
-                      ),
-                      placeholder: 'Title',
-                      controller: nameController,
-                      keyboardType: TextInputType.text,
-                      // clearButtonMode: OverlayVisibilityMode.editing,
-                      style: GoogleFonts.poppins(),
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 16, vertical: 6),
+                  const SizedBox(height: 20.0),
+                  CupertinoTextField(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5.0),
+                      color: Colors.white,
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.grey, // You can set the shadow color
+                          offset:
+                              Offset(0, 1), // Specify the offset of the shadow
+                          blurRadius: 4, // Specify the blur radius
+                        ),
+                      ],
                     ),
-                    const SizedBox(height: 20.0),
-                    CupertinoTextField(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(30.0),
-                        color: Colors.white,
-                      ),
-                      placeholder: 'Description',
-                      controller: emailController,
-                      keyboardType: TextInputType.text,
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 16,
-                          vertical: 6), // Adjust the horizontal padding here
-                      clearButtonMode: OverlayVisibilityMode.editing,
-                      style: GoogleFonts.poppins(),
+                    placeholder: 'Description',
+                    controller: emailController,
+                    keyboardType: TextInputType.text,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 6,
                     ),
-                    const SizedBox(height: 20.0),
-                    Container(
-                      height: 40,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(30),
-                        color: Colors.white,
+                    clearButtonMode: OverlayVisibilityMode.editing,
+                    style: GoogleFonts.poppins(),
+                  ),
+                  const SizedBox(height: 20.0),
+                  Container(
+                    height: 40,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5.0),
+                      color: Colors.white,
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.grey, // You can set the shadow color
+                          offset:
+                              Offset(0, 1), // Specify the offset of the shadow
+                          blurRadius: 4, // Specify the blur radius
+                        ),
+                      ],
+                    ),
+                    child: DropdownSearch<String>(
+                      popupProps: PopupProps.menu(
+                        constraints: const BoxConstraints.tightFor(
+                          height: 300,
+                        ),
+                        showSearchBox: true,
+                        searchDelay: Duration.zero,
+                        showSelectedItems: true,
+                        containerBuilder: (ctx, popupWidget) {
+                          return Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(5.0),
+                            ),
+                            child: popupWidget,
+                          );
+                        },
                       ),
-                      child: DropdownSearch<String>(
-                        popupProps: PopupProps.menu(
-                          constraints: const BoxConstraints.tightFor(
-                            height: 300,
-                          ),
-                          showSearchBox: true,
-                          searchDelay: Duration.zero,
-                          showSelectedItems: true,
-                          // disabledItemFn: (String s) => s.startsWith('I'),
-                          // itemBuilder: _customPopupItemBuilder,
-                          containerBuilder: (ctx, popupWidget) {
-                            return Container(
-                              decoration: BoxDecoration(
-                                color: Colors.red,
-                                borderRadius: BorderRadius.circular(
-                                    30.0), // Adjust the radius as needed
+                      items: const [
+                        "MYR",
+                        "USD",
+                        "EUR",
+                      ],
+                      dropdownDecoratorProps: const DropDownDecoratorProps(
+                        dropdownSearchDecoration: InputDecoration(
+                          hintText: "Select currency",
+                          border: InputBorder.none,
+                          contentPadding: EdgeInsets.only(left: 16, top: 3),
+                        ),
+                      ),
+                      onChanged: print,
+                    ),
+                  ),
+                  const SizedBox(height: 20.0),
+                  Expanded(
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: items.length + 1,
+                      itemBuilder: (context, index) {
+                        if (index < items.length) {
+                          return ListTile(
+                            contentPadding: const EdgeInsets.only(left: 20.0),
+                            title: Text(items[index]),
+                            trailing: IconButton(
+                              icon: const Icon(Icons.clear),
+                              onPressed: () {
+                                removeMember(index);
+                              },
+                            ),
+                          );
+                        } else {
+                          return ListTile(
+                            contentPadding: const EdgeInsets.only(left: 20.0),
+                            title: TextField(
+                              controller: textController,
+                              decoration: const InputDecoration(
+                                hintText: 'Enter name', // Add hint text here
                               ),
-                              child: popupWidget,
-                            );
-                          },
-                        ),
-                        items: const [
-                          "Brazil",
-                          "Italia (Disabled)",
-                          "Tunisia",
-                          'Canada',
-                          'Canadaa',
-                          'Canadaaa',
-                          'Canadaaaa',
-                        ],
-                        dropdownDecoratorProps: const DropDownDecoratorProps(
-                          dropdownSearchDecoration: InputDecoration(
-                            hintText: "Select country",
-                            border: InputBorder.none,
-                            contentPadding: EdgeInsets.only(left: 16, top: 3),
-                          ),
-                        ),
-                        onChanged: print,
-                        // selectedItem: "Brazil",
-                      ),
+                              // decoration: BoxDecoration(
+                              //   borderRadius: BorderRadius.circular(30.0),
+                              //   color: Colors.white,
+                              // ),
+                            ),
+                            trailing: Padding(
+                              padding: const EdgeInsets.only(
+                                  right: 0.0), // Adjust the padding value here
+                              child: IconButton(
+                                icon: const Icon(Icons.add),
+                                onPressed: () {
+                                  addMember(textController.text);
+                                },
+                              ),
+                            ),
+                          );
+                        }
+                      },
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ],
       ),
